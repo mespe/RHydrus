d97bf168e438403fc3306adc47406034e924093f
Duncan Temple Lang <duncan@r-project.org>
2017-07-05 16:50:15 -0700

    for now strdup these. We'll remove this later

--- src/Rhydrus.c
+++ src/Rhydrus.c
@@ -13,7 +13,7 @@ R_hydrus_standalone(SEXP r_args)
     xargc = Rf_length(r_args);
     xargv = (char **) R_alloc(sizeof(char *), xargc);
     for(int i = 0; i < xargc; i++)
-	xargv[i] = CHAR(STRING_ELT(r_args, i));
+	xargv[i] = strdup(CHAR(STRING_ELT(r_args, i)));
     MAIN__();
     return(R_NilValue);
 }
